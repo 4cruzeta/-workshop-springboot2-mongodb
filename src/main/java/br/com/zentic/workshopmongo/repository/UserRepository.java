294b7253c2fa63e67991276c71f6db7d0cd434dc
4cruzeta <4cruzetam@gmail.com>
2020-01-30 15:45:30 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/br/com/zentic/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package br.com.zentic.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.zentic.workshopmongo.domain.User;
+
+	
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
