b3d488b90e69f5b57b573aa96e57a0a765d489e5
4cruzeta <4cruzetam@gmail.com>
2020-01-31 14:29:30 -0300

    Users - Insere com Post

--- src/main/java/br/com/zentic/workshopmongo/services/UserService.java
+++ src/main/java/br/com/zentic/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.zentic.workshopmongo.domain.User;
+import br.com.zentic.workshopmongo.dto.UserDTO;
 import br.com.zentic.workshopmongo.repository.UserRepository;
 import br.com.zentic.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
